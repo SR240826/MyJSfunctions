1abf9824aae2f0749c44aa4743f62fb888856c6b
root <root@DESKTOP-LAESVCH>
2024-01-22 23:46:45 +0600

    Changed the player creation

--- createPlayer.js
+++ createPlayer.js
@@ -1,23 +1,26 @@
-class player { 
-    #playerHeight;
-    #playerWidth;
-    #ctx;
-    #playerPosX;
-    #playerPosY;
-    #color;
-    constructor(ctx, playerPosX, playerPosY, playerWidth, playerHeight, color) {
+export class Player {
+    #canvas; // main canvas ID
+    #ctx; // canvas.getContext('2d');
+    constructor(canvas, ctx, color) {
         this.#ctx = ctx;
-        this.#playerPosX = playerPosX;
-        this.#playerPosY = playerPosY;
-        this.#playerWidth = playerWidth;
-        this.#playerHeight = playerHeight;
-        this.#color = color;
+        this.#canvas = canvas;
+        this.size = {
+            width: canvas.width/30,
+            height: canvas.width/30
+        }
+        this.position = {
+            x: Math.random() * (canvas.width - this.size.width) + 5,
+            y: Math.random() * (canvas.height - this.size.height* 2) + 60,
+        }
+        this.color = color;
     }
-    draw () {
-        this.#ctx.fillStyle = this.#color;
-        this.#ctx.fillRect(this.#playerPosX, this.#playerPosY, this.#playerWidth, this.#playerHeight) // This will draw a player
-    }
-    update() {
-
+    draw() {
+        this.#ctx.fillStyle = this.color;
+        this.#ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+        if (this.position.y > canvas.height - this.size.height) {
+            this.#ctx.clearRect(this.position.x, this.position.y, this.size.width, this.size.height);
+            this.position.y = Math.random() * (canvas.height - this.size.height* 2) + 60;
+            this.draw()
+        }
     }
 }
