d3eeb4db2f527e7b9fcba604098ffc6433c40b90
sanjidur <sanjidur@gmail.com>
2024-01-24 03:26:25 +0600

    modified player creation

--- createPlayer.js
+++ createPlayer.js
@@ -5,22 +5,27 @@ export class Player {
         this.#ctx = ctx;
         this.#canvas = canvas;
         this.size = {
-            width: canvas.width/30,
-            height: canvas.width/30
+            width: this.#canvas.width/30,
+            height: this.#canvas.width/30
         }
         this.position = {
-            x: Math.random() * (canvas.width - this.size.width) + 5,
-            y: Math.random() * (canvas.height - this.size.height* 2) + 60,
+            x: Math.random() * (this.#canvas.width - this.size.width) + 5,
+            y: Math.random() * (this.#canvas.height - this.size.height* 2) + 60,
         }
         this.color = color;
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
     }
     draw() {
         this.#ctx.fillStyle = this.color;
         this.#ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-        if (this.position.y > canvas.height - this.size.height) {
-            this.#ctx.clearRect(this.position.x, this.position.y, this.size.width, this.size.height);
-            this.position.y = Math.random() * (canvas.height - this.size.height* 2) + 60;
-            this.draw()
-        }
+        
+    }
+    update(velocityX, velocityY) {
+        this.draw();
+        this.position.x += velocityX;
+        this.position.y += velocityY;
     }
 }
